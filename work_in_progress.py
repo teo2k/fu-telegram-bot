d18ac9f0e522c16e204837ba530c0609cfdb56ff
teo2k <64436549+teo2k@users.noreply.github.com>
2022-12-08 16:44:25 +0300

    Update work_in_progress.py

--- work_in_progress.py
+++ work_in_progress.py
@@ -1,4 +1,5 @@
 import telebot
+import requests
 bot = telebot.TeleBot('5820580999:AAEl6HI68CTWPhkSCExCZbN_3sJ13UQieTc')
 
 @bot.message_handler(commands=['start'])
@@ -6,6 +7,12 @@ def start(message):
     bot.send_message(message.chat.id,
                      text="Привет, {0.first_name}! К сожалению в данный момент ведутся технические работы над ботом. Попробуй чуть позже.".format(
                          message.from_user))
+params = {
+    'chat_id': '-1001504118384',
+    'text': 'ATTENTION БОТ УПАЛ',
+}
+response = requests.get('https://api.telegram.org/bot'+TOKEN+'/sendMessage', params=params)
+
 @bot.message_handler(content_types=['text'])
 def func(message):
     if (message.text == ""):
